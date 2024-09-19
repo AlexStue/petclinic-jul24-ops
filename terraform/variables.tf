7e7c4befe502fb7225c1d2c3993022af14138da2
AlexStue <alexstue@gmx.de>
2024-09-19 13:11:16 +0200

    DRAFT. GHActions DTS-Server rollout

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "server_ip_tf" {
   description = "Public IP of the Ubuntu server where K3s will be installed."
-  default     = "18.201.22.142"
+  default     = "34.241.81.53"
 }
 
 variable "ssh_user" {
