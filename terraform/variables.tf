845e76c347815206c45e90fb295d5879aee593c5
Rustingheart <hierzi.p@gmail.com>
2024-09-26 17:09:42 +0200

    update serverip

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "server_ip_tf" {
   description = "Public IP of the Ubuntu server where K3s will be installed."
-  default     = "52.48.63.244"
+  default     = "52.16.155.59"
 }
 
 variable "ssh_user" {
