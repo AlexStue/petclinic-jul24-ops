8dca0509b7364dd4e1dc4910ac0ce9aa741b9ab0
Rustingheart <hierzi.p@gmail.com>
2024-10-04 00:08:12 +0200

    bugfix chmod +x

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,31 @@
 provider "local" {}
 
+resource "null_resource" "make_scripts_executable" {
+  provisioner "local-exec" {
+    command = "chmod +x ~/petclinic-jul24-ops/scripts/install_k3s.sh"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+resource "null_resource" "make_app_script_executable" {
+  provisioner "local-exec" {
+    command = "chmod +x ~/petclinic-jul24-ops/scripts/deploy_k3s_app.sh"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
 resource "null_resource" "k3s_setup" {
   provisioner "local-exec" {
     command = "bash ~/petclinic-jul24-ops/scripts/install_k3s.sh"
   }
+
+  depends_on = [null_resource.make_scripts_executable]
 
   triggers = {
     always_run = "${timestamp()}"
@@ -15,5 +37,5 @@ resource "null_resource" "apply_k3s_deployment" {
     command = "bash ~/petclinic-jul24-ops/scripts/deploy_k3s_app.sh"
   }
 
-  depends_on = [null_resource.k3s_setup]
+  depends_on = [null_resource.make_app_script_executable, null_resource.k3s_setup]
 }
