c757ff12c2d42926d89a0d9ee4181ad40bc9434a
Rustingheart <hierzi.p@gmail.com>
2024-10-03 23:09:50 +0200

    updated ingress in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,18 @@ resource "null_resource" "k3s_setup" {
       echo "Provisioning local Ubuntu machine"
       sudo apt-get update
       sudo apt-get install -y curl
-      curl -sfL https://get.k3s.io | sh -
+      curl -sfL https://get.k3s.io | sh - --disable-network-policy --disable-edit-support --disable-cloud-controller
       mkdir -p ~/.kube
       sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
       sudo chown $USER:$USER ~/.kube/config
       sudo chmod 644 ~/.kube/config
       sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+      
+      # Disable the built-in Traefik
+      kubectl delete --ignore-not-found -n kube-system deployment traefik
+
+      # Install NGINX Ingress Controller
+      kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
     EOT
   }
 
