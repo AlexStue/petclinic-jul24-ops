6ece27adc5e063362a092450432d968a3f5d67f1
Rustingheart <hierzi.p@gmail.com>
2024-10-04 17:55:05 +0200

    added tls-secret.yml 1.0

--- scripts/deploy_minikube_app.sh
+++ scripts/deploy_minikube_app.sh
@@ -5,5 +5,4 @@ echo "Applying Kubernetes deployment"
 kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/petclinic-combined.yml
 kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/nginx-combined.yml
 kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/dashboard-ingress.yml
-kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/letsencrypt-clusterissuer.yml
-kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/cert.yml
+kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/tls-secret.yml
