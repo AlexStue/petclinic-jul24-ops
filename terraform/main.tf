1620d83498a04332df4fb980223b5fcfd623979e
Rustingheart <hierzi.p@gmail.com>
2024-10-04 15:00:36 +0200

    bugfix 3.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,8 @@ command = "chmod +x ~/petclinic-jul24-ops/scripts/install_minikube.sh"
 
 resource "null_resource" "make_app_script_executable" {
   provisioner "local-exec" {
-    command = "chmod +x ~/petclinic-jul24-ops/scripts/deploy_k3s_app_mkube.sh"
+command = "chmod +x ~/petclinic-jul24-ops/scripts/deploy_minikube_app.sh"
+  #  command = "chmod +x ~/petclinic-jul24-ops/scripts/deploy_k3s_app.sh"
   }
 
   triggers = {
@@ -36,11 +37,11 @@ resource "null_resource" "k3s_setup" {
 
 resource "null_resource" "apply_k3s_deployment" {
   provisioner "local-exec" {
-    command = "bash ~/petclinic-jul24-ops/scripts/deploy_k3s_app_mkube.sh"
+    command = "bash ~/petclinic-jul24-ops/scripts/deploy_minikube_app.sh"
   #  command = "bash ~/petclinic-jul24-ops/scripts/deploy_k3s_app.sh"
   }
-
-  depends_on = [null_resource.make_app_script_executable, null_resource.k3s_setup]
+depends_on = [null_resource.make_app_script_executable, null_resource.minikube_setup]
+#  depends_on = [null_resource.make_app_script_executable, null_resource.mk3s_setup]
 }
 
 resource "null_resource" "run_script" {
