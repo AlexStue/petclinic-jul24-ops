51a513e61695a192b9206dc600b69c0a184f1312
Rustingheart <hierzi.p@gmail.com>
2024-10-04 15:13:15 +0200

    bugfix deploy-sh 5.0

--- scripts/deploy_minikube_app.sh
+++ scripts/deploy_minikube_app.sh
@@ -2,8 +2,8 @@
 set -e # Exit on any error
 
 echo "Applying Kubernetes deployment"
-kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s_mkube/petclinic-combined.yml
-kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s_mkube/nginx-combined.yml
-# kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s_mkube/dashboard-ingress.yml
-# kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s_mkube/letsencrypt-clusterissuer.yml
-# kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s_mkube/cert.yml
+kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/petclinic-combined.yml
+kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/nginx-combined.yml
+# kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/dashboard-ingress.yml
+# kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/letsencrypt-clusterissuer.yml
+# kubectl apply -f /home/ubuntu/petclinic-jul24-ops/minikube/cert.yml
