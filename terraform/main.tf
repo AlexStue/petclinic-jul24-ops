f0ebfee8bf47838d6c378b381b582a3bc00d923a
Rustingheart <hierzi.p@gmail.com>
2024-10-06 21:18:25 +0200

    bugfix 3.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "local" {}
 resource "null_resource" "make_scripts_executable" {
   provisioner "local-exec" {
     # command = "chmod +x ~/petclinic-jul24-ops/scripts/install_minikube.sh"
-    command = "chmod +x ~/petclinic-jul24-ops/scripts/install_k3s.sh"
+    command = "chmod +x ~/petclinic-jul24-ops/scripts/install_k3s_app.sh"
   }
 
   triggers = {
@@ -26,7 +26,7 @@ resource "null_resource" "make_app_script_executable" {
 resource "null_resource" "k3s_setup" {
   provisioner "local-exec" {
     # command = "bash ~/petclinic-jul24-ops/scripts/install_minikube.sh"
-    command = "bash ~/petclinic-jul24-ops/scripts/install_k3s.sh"
+    command = "bash ~/petclinic-jul24-ops/scripts/install_k3s_app.sh"
   }
 
   depends_on = [null_resource.make_scripts_executable]
@@ -39,8 +39,9 @@ resource "null_resource" "k3s_setup" {
 # resource "null_resource" "apply_minikube_deployment" {
 resource "null_resource" "apply_k3s_deployment" {
   provisioner "local-exec" {
-    # command = "bash ~/petclinic-jul24-ops/scripts/deploy_k3s_app.sh"
-    command = "bash ~/petclinic-jul24-ops/scripts/deploy_minikube_app.sh"
+
+    # command = "bash ~/petclinic-jul24-ops/scripts/deploy_minikube_app.sh"
+    command = "bash ~/petclinic-jul24-ops/scripts/deploy_k3s_app.sh"
   }
 
   # depends_on = [null_resource.make_app_script_executable, null_resource.minikube_setup]
@@ -49,7 +50,7 @@ resource "null_resource" "apply_k3s_deployment" {
 
 resource "null_resource" "run_script" {
   provisioner "local-exec" {
-    command = "bash ~/petclinic-jul24-ops/scripts/deploy-on-dts.sh"  # Replace with the actual path to your_script.sh
+    command = "bash ~/petclinic-jul24-ops/scripts/deploy-on-dst.sh"  # Replace with the actual path to your_script.sh
   }
   
   # optionally use triggers if you want to force rerun
