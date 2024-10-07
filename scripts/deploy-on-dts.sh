091645a0e8cfc1024b6be9676d84c9ddfbb279ed
Rustingheart <hierzi.p@gmail.com~>
2024-10-07 14:21:39 +0200

    back to commit 1c34f814258f74ee4f81c2fc64f431a957d75bfe /only petclinic-combined.yml is used

--- scripts/deploy-on-dts.sh
+++ scripts/deploy-on-dts.sh
@@ -10,13 +10,13 @@ handle_lock() {
   local retries=10
   local wait_time=10  # in seconds
 
-  while [ $retries -gt 0 ]; do # errors vefore ?!
+  while [ $retries -gt 0 ]; do
     if sudo lsof $lock_file; then
       echo "Lock file $lock_file is held by another process. Retrying in $wait_time seconds..."
       sleep $wait_time
       retries=$((retries - 1))
     else
-      return 0 # errors before?!
+      return 0
     fi
   done
 
@@ -25,17 +25,13 @@ handle_lock() {
 }
 
 # Step 1: Install Terraform on the remote server
-echo "Adding HashiCorp GPG key..."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-echo "Adding HashiCorp repository..."
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com focal main"
 
 # Handle apt-get lock
 handle_lock "/var/lib/dpkg/lock-frontend"
 
-echo "Updating package lists..."
 sudo apt-get update
-echo "Installing Terraform..."
 sudo apt-get install -y terraform
 
 # Step 2: Clone the repository containing the Terraform code
@@ -53,11 +49,8 @@ fi
 
 # Step 3: Navigate to the Terraform directory and deploy infrastructure
 cd /home/ubuntu/petclinic-jul24-ops/terraform
-echo "Initializing Terraform..."
 terraform init
-echo "Planning Terraform deployment..."
 terraform plan
-echo "Applying Terraform deployment..."
 terraform apply -auto-approve
 
-echo "Deployment completed successfully."
+# After this, your application should be up and running on the server.
