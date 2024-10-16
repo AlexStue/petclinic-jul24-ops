ad972245261bfed3acf21d6d9f82fb605f876726
Rustingheart <hierzi.p@gmail.com>
2024-10-16 22:36:01 +0200

    additional mysql bg 1.2

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,7 @@ resource "null_resource" "apply_k3s_deployment" {
       kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s/ingress/tls-secret.yml
       kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s/ingress/petclinic-combined+.yml
       kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s/ingress/ingress-traefik.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s/db/db-configmap.yml
       kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s/db/db-secret.yml
       kubectl apply -f /home/ubuntu/petclinic-jul24-ops/k3s/db/db-mysql.yml
 
